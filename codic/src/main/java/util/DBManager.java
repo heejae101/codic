6b63c488cfdf9b870b8bb6980361ab43275d485d
heejae101 <heejae0629@naver.com>
2023-07-04 16:49:19 +0900

    init

---
+++ codic/src/main/java/util/DBManager.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBManager {
+	public static Connection getConnection() {
+		Connection conn = null;
+		String url = "jdbc:mysql://database-1.cydw0hkrffj9.ap-northeast-2.rds.amazonaws.com:3306/CODIC?serverTimeZone=UTC";
+		String username = "admin";
+		String password = "0*uqeSic#mu8rIta4iWr";		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Database 연동 성공 >>>");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Database 연동 실패 >>>");
+		}
+		return conn;
+	}
+}
