2ff416006c133f0430b98429cd303e2ebca5ab69
heejae101 <heejae0629@naver.com>
2023-07-05 12:24:37 +0900

    modified join.sql

--- codic/src/main/webapp/database/join.sql
+++ codic/src/main/webapp/database/join.sql
@@ -1,19 +1,38 @@
 create database `CODIC`;
-
 use `CODIC`;
-CREATE TABLE `user_info` (
-	`user_email`	varchar(20)	NOT NULL,
-	`user_password`	varchar(20)	NULL,
-	`user_name`	varchar(20)	NULL,
-	`user_phone_num`	int(15)	NULL,
-	`user_nickname`	varchar(14)	NULL,
-	`user_check`	int(1)	NULL,
-	`user_joindate`	timestamp	NULL,
-	`user_status`	int(1)	NULL
-);
+/*
+user_email 
+RFC 2822 에 정의되어 있는 길이 64(주소부분) + @ + 255(도매인부분) = 320 이기 때문에 320으로 길이 제한을 둡니다.  
+기능: 중복체크와 id 대신 사용합니다. 
 
-ALTER TABLE `user_info` ADD CONSTRAINT `PK_USER_INFO` PRIMARY KEY (
-	`user_email`
+user_password 
+비밀번호 길이는 8자 이상이어야 하며, 15자리 이내로 지정합니다. 영문 대/소문자, 숫자, 특수문자 중 3종류 이상을 혼합하여 사용해야 합니다.
+
+user_name 
+
+user_phone_num 
+XXX-XXXX-XXXX 포멧으로 15자리 제한을 둡니다.
+
+user_nickname
+
+user_check 
+14세 미만 동의 1 동의 안할시 0
+
+user_joindate 
+
+user_status 
+휴면상태  : 0 ,정상 : 1, 탈퇴상태 : 2
+*/
+
+CREATE TABLE `user_info` (
+  `user_email` varchar(320) NOT NULL primary key,
+  `user_password` varchar(15) NOT NULL,
+  `user_name` varchar(15) NOT NULL,
+  `user_phone_num` varchar(15) NOT NULL,
+  `user_nickname` varchar(14) NOT NULL,
+  `user_check` int NOT NULL,
+  `user_joindate` timestamp NOT NULL default current_timestamp,
+  `user_status` int NOT NULL
 );
 
 select * from `user_info`;
