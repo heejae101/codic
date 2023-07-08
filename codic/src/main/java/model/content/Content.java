113217a6178a20b8e13251ff03b0988bdca2170a
heejae101 <heejae0629@naver.com>
2023-07-08 22:52:42 +0900

    modified Content.java

--- codic/src/main/java/model/content/Content.java
+++ codic/src/main/java/model/content/Content.java
@@ -18,49 +18,33 @@ public class Content {
 		return category_name;
 	}
 
-
-
 	public void setCategory_name(String category_name) {
 		this.category_name = category_name;
 	}
-
-
 
 	public String getContent_title() {
 		return content_title;
 	}
 
-
-
 	public void setContent_title(String content_title) {
 		this.content_title = content_title;
 	}
-
-
 
 	public int getContent_views() {
 		return content_views;
 	}
 
-
-
 	public void setContent_views(int content_views) {
 		this.content_views = content_views;
 	}
-
-
 
 	public String getFile_path() {
 		return file_path;
 	}
 
-
-
 	public void setFile_path(String file_path) {
 		this.file_path = file_path;
 	}
-
-
 
 	@Override
 	public String toString() {
